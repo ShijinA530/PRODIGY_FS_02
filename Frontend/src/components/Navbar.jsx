e170ee0ad998f3c8d927b63a6c947b3ec1f1268c
Shijin A <shijinasasi835@gmail.com>
2024-08-03 10:57:45 +0530

    heading cruds added

--- Frontend/src/components/Navbar.jsx
+++ Frontend/src/components/Navbar.jsx
@@ -4,7 +4,7 @@ const Navbar = () => {
   return (
     <header className='bg-gray-400'>
         <div className='max-w-[1400px] mx-auto py-6 flex justify-center'>
-            <h1 className='text-white text-3xl font-semibold'>Personal Data Collection</h1>
+            <h1 className='text-white text-3xl font-semibold'>CRUDS Demo</h1>
         </div>
     </header>
   )
