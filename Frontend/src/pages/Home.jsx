5a352a9be6128ba100ea726e3ee0f32bbeccb395
ShijinA530 <shijinasasi835@gmail.com>
2024-09-12 11:47:59 +0530

    loading component added

--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -1,11 +1,15 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import axios from 'axios'
 import PopupForm from '../components/PopupForm'
 import Table from '../components/Table'
 import { useDataContext } from '../hooks/useDataContext'
+import { ClipLoader } from 'react-spinners'
+
 
 export default function Home() {
   const { data, dispatch } = useDataContext()
+
+  const [loading, setLoading] = useState(true)
 
   useEffect(() => {
     const fetchData = async () => {
@@ -14,14 +18,24 @@ export default function Home() {
         const json = response.data
         
         dispatch({type: 'SET_DATA', payload: json})
+        setLoading(false)
 
       } catch (err) {
         console.log({error: err.message});
+        setLoading(false)
       }
     }
 
     fetchData()
   }, [])
+
+  if (loading) {
+    return (
+      <div className='flex justify-center items-center h-full'>
+        <ClipLoader color="#3498db" loading={true} size={50} />
+      </div>
+    )
+  }  
 
   return (
     <>
