674fd54d7e8a8a2d8e9c3b15c1749c7bc736ef5b
ShijinA530 <shijinasasi835@gmail.com>
2024-09-12 13:49:31 +0530

    okk

--- Frontend/src/axios/axios.js
+++ Frontend/src/axios/axios.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 // Configure axios with the base URL
-const axios = axios.create({
+const Axios = axios.create({
   baseURL: 'https://prodigy-fs-02-0n0p.onrender.com'
 });
 
-export default axios;
+export default Axios;
